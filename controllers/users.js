79b48a3f384dfd91e804197793ff8042f8a1477f
Ivan <vany4golos@yandex.ru>
2023-12-21 17:10:31 +0300

    test

--- controllers/users.js
+++ controllers/users.js
@@ -39,7 +39,7 @@ module.exports.updateUserInfo = async(req, res) => {
   const { name, about } = req.body;
   try {
     const user = await User.findByIdAndUpdate(req.params.userId,
-      {name, about },
+      { name, about },
       {new: true});
       if (!user) {
         return res.status(400).json({ message: 'Пользователь не найден' });
@@ -48,6 +48,9 @@ module.exports.updateUserInfo = async(req, res) => {
   } catch (err) {
     if (err.name === 'CastError') {
       return res.status(400).json({ message: 'некорректный id' })
+    }
+    if(err.name === 'ValidationError') {
+      return res.status(400).json({ message: 'неправильный ввод' });
     }
     res.status(400).json({ message: err.message });
   }
