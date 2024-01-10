61061d6367e54d4fc846eb89936bac5bde70b877
Ivan <vany4golos@yandex.ru>
2024-01-10 19:01:53 +0300

    delete create

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const { getUser, createUser, getUserById, updateUserInfo, updateAvatar } = require('../controllers/users');
+const { getUser, getUserById, updateUserInfo, updateAvatar } = require('../controllers/users');
 
 router.get('/users', getUser);
 router.get('/users/:userId', getUserById);
-router.post('/users', createUser);
+
 router.patch('/users/me', updateUserInfo);
 router.patch('/users/me/avatar', updateAvatar);
+
 
 module.exports = router;
