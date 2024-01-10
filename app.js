f7748b01cd323f26ae546f745e41fbbbff0a7cf0
Ivan <vany4golos@yandex.ru>
2024-01-10 19:02:03 +0300

    fix app

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const { PORT = 3000 } = process.env;
 const express = require('express');
 const mongoose = require('mongoose');
+const auth = require('./middlewares/auth.js');
 
 const userRoutes = require('./routes/users');
 const cardRoutes = require('./routes/cards');
+const login = require('./controllers/users.js');
+const createUser = require('./controllers/users.js');
 
 const app = express();
 
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
-
-app.use((req, res, next) => {
-  req.user = {
-    _id: '6581885957442f88660fbab6',
-  };
-
-  next();
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
-app.use('/', userRoutes);
+app.use('/signup', createUser);
+app.use('/signin', login);
 
+app.use(auth);
+
+app.use('/', userRoutes);
 app.use('/', cardRoutes);
 
 app.use((req, res) => {
