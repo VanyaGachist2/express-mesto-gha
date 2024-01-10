232387c6ab0e0e63a9419d2928b99ac47dd118c1
Ivan <vany4golos@yandex.ru>
2024-01-10 19:01:40 +0300

    fix model user

--- models/user.js
+++ models/user.js
@@ -1,27 +1,64 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const { default: isEmail } = require('validator/lib/isEmail');
+const { default: isURL } = require('validator/lib/isURL');
+const bcrypt = require('bcrypt');
+
+const regularAvatar = /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/;
 
 const users = mongoose.Schema({
   name: {
     type: String,
-    required: {value: true, message: 'name должен быть заполнен'},
-    minlength: [2, 'минимальная длинна - 2'],
-    maxlength: [30, 'максимальная длинна - 30'],
+    minlength: 2,
+    maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: {value: true, message: 'about должен быть заполнен'},
-    minlength: [2, 'минимальная длина - 2'],
-    maxlength: [30, 'максимальная длинна - 30'],
+    minlength: 2,
+    maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
     validate: {
-      validator: (u) => validator.isURL(u),
-      message: 'некорректный url'
+      validator: (u) => isURL(u) && regularAvatar,
+      message: 'неправильная ссылка на аватар'
     },
-    required: {value: true, message: 'avatar должен быть заполнен'},
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: {value: true, message: 'Поле email должно быть заполнено'},
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Некорректные данные'
+    },
+  },
+  password: {
+    type: String,
+    required: {value: true, message: 'поле password должно быть заполнено'},
+    minlength: 8,
+    select: false,
   },
 });
+
+users.statics.findUserByCredentials = function(email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+      if(!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if(!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+
+          return user;
+        })
+    })
+}
 
 module.exports = new mongoose.model('user', users);
