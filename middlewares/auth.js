43a85d3383e9e4635825aa5e5b546871c4a6138e
Ivan <vany4golos@yandex.ru>
2024-01-12 14:13:07 +0300

    fix auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,29 +1,17 @@
 const jwt = require('jsonwebtoken');
 
-const handleAuthError = (req, res, next) => {
-  res.status(401).json({ message: 'Необходима авторизация' });
-}
-
-const extractBearerToken = (header) => {
-  return header.replace('Bearer ', '');
-}
-
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return handleAuthError(res);
+    return res.status(401).send({ message: 'необходима регистрация' });
   }
-  const token = extractBearerToken(authorization);
+  const token = authorization.replace('Bearer ', '');
   let payload;
-
   try {
     payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
-    return handleAuthError(res);
+    return next(res.status(401).send({ message: 'необходима авторизация'}));
   }
-
   req.user = payload;
-
-  next(); 
-}
+  next();
+};
