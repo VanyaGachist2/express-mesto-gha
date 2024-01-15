22b234d7b00cd55ecb40f1de5450fc02543a8c75
Ivan <vany4golos@yandex.ru>
2024-01-15 23:10:57 +0300

    add select

--- controllers/users.js
+++ controllers/users.js
@@ -4,24 +4,9 @@ const bcrypt = require('bcrypt');
 
 module.exports.getUser = async(req, res) => {
   try {
-    const users = await User.find({});
+    const users = await User.find({}).select('-password');
     return res.status(200).json(users);
   } catch (err) {
-    return res.status(500).json({ message: err.message });
-  }
-}
-
-module.exports.getOneUser = async(req, res) => {
-  try {
-    const user = await User.findById(req.params._id).select('-password');
-    if(!user) {
-      return res.status(404).json({ message: 'такого пользователя нет' });
-    }
-    return res.status(200).json(user);
-  } catch(err) {
-    if(err.name === 'CastError') {
-      return res.status(400).json({ message: 'неккоректный id' });
-    }
     return res.status(500).json({ message: err.message });
   }
 }
